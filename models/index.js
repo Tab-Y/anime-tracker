3f1201f158b2d7d99db087c93c8cb4b846a494f6
Taber Youmans <tlyoumans@gmail.com>
2022-11-14 15:19:55 -0500

    Folders/files (#42)
    * basic file and folder structure
    
    * seed data to be refactored to the project
    
    * some npm packages
    
    * npm packages
    
    * npm animate.css
    
    * readme structure
    
    * pallette img
    
    * seed data for tables
    
    * seed data for tables
    
    * updated data for seeding
    
    * removed id from tags to auto incriment
    
    * user seed data
    
    * models and seeds
    
    * seeds updated to be inserted into DB, DB set to pull tables from models and seed
    
    * seeder and models working and seeding DB

--- models/index.js
+++ models/index.js
@@ -1,12 +1,28 @@
 const Main = require('./Main');
 const Tags = require('./Tags');
 const Users = require('./Users');
+const TagId = require('./TagId');
+
 
 // User.hasMany(Favorites, {  ...   })
 // User.hasMany(Watching, {   ...   })
 // User.hasMany(CurrentlyWatching {  ...  })
 // User.hasMany(Completed, { ... })
 
+Tags.belongsToMany(Main, {
+    through: {
+        model: TagId,
+        unique: false
+    },
+    foreignkey: 'tags_id',
+});
 
+Main.belongsToMany(Tags, {
+    through: {
+        model: TagId,
+        unique: false
+    },
+    foreignkey: 'title_id',
+});
 
-module.exports = { Main, Tags, Users }
+module.exports = { Main, Tags, Users, TagId }
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,16 +1,20 @@
 const sequelize = require('../config/connection');
-const { Tags, Main, Users } = require('../models');
+const { Tags, Main, Users, TagId } = require('../models');
+const tagIdSeed = require('./tagId');
 const tagSeeds = require('./tags');
 const mainSeeds = require('./title');
-const userSeeds =require('./userSeed');
+const userSeeds = require('./userSeed');
+
+
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   
   await userSeeds();
-  await tagSeeds();
-
   await mainSeeds();
+  await tagSeeds();
+  await tagIdSeed();
+  
 
 
   process.exit(0);
