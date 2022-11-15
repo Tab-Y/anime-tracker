e63234a9b33e3cf49732e134f445c7b6cdec7e9a
Taber Youmans <tlyoumans@gmail.com>
2022-11-15 11:04:52 -0500

    Folders/files (#48)
    * basic file and folder structure
    
    * seed data to be refactored to the project
    
    * some npm packages
    
    * npm packages
    
    * npm animate.css
    
    * readme structure
    
    * pallette img
    
    * seed data for tables
    
    * seed data for tables
    
    * updated data for seeding
    
    * removed id from tags to auto incriment
    
    * user seed data
    
    * models and seeds
    
    * seeds updated to be inserted into DB, DB set to pull tables from models and seed
    
    * seeder and models working and seeding DB
    
    * added file for splash page
    
    * controller structure
    
    * fixed small refrencing bug in tags to tagid
    
    * fixed bug of every entry having a none tag
    
    * app server linked and tested to routes, basic render on the 4 linked routes

--- controllers/api/favorites-routes.js
+++ controllers/api/favorites-routes.js
@@ -1,2 +1,11 @@
-// gets favorites from db
-// renders favorites tab
+const router = require('express').Router();
+const { Main, TagId, Tags, Users } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+  res.render('login', { 
+
+  });
+  });
+
+  module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,11 @@
-// splash page
-// login
-// signup
-// other things without access
+const router = require('express').Router();
+const { Main, TagId, Tags, Users } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  res.render('login', { 
+
+  });
+  });
+
+  module.exports = router;
