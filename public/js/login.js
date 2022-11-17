2a8590b40879595be6c62c7df0a22a9bf0b79fd7
Taber Youmans <tlyoumans@gmail.com>
2022-11-16 22:26:56 -0500

    Feature/routes (#93)
    * added login routing and form capture
    
    * user favorites database table setup
    
    * status table added
    
    * routes for login, deleting favorites (list of tracking), finding favorites by id
    
    * splash page format and linking into log-in and discovery
    
    * removed conflict
    
    * splash page fine tuning
    
    * removed conflict in splash
    
    * update to discover route
    
    * splash page update
    
    * splash css
    
    * readme update
    
    * added comments to seeds
    
    * conflict resolution
    
    * discover routes
    
    * discover routes update
    
    * api/profile route working
    
    * updated profile route to call user's favs
    
    * create and delete favs

--- public/js/login.js
+++ public/js/login.js
@@ -16,35 +16,7 @@ const loginFormHandler = async (event) => {
 
         // When login form is correct user is redirected in their profile page
         if (response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert(response.statusText);
-        }
-    }
-};
-
-
-// New user Sign-Up form
-const signupFormHandler = async (event) => {
-    event.preventDefault();
-
-    // Collecting sign-up form values
-    const email = document.querySelector('#email-signup').value.trim();
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-    // const password = document.querySelector('#pass-sign').value.trim();
-
-    // A POST request that sends off to API
-    if (email && username && password) {
-        const response = await fetch('/api/user', {
-            method: 'POST',
-            body: JSON.stringify({ email, username, password}),
-            headers: { 'Content-Type': 'application/json'},
-        });
-
-        // When sign-up is sucessful new user is redirected to the profile page
-        if (response.ok) {
-            document.location.replace('/profile');
+            document.location.replace('/api/discover');
         } else {
             alert(response.statusText);
         }
@@ -56,8 +28,5 @@ const signupFormHandler = async (event) => {
 // Submission click-on listners
 // Login submit form
 document
-    .querySelector('.logged-form')
+    .querySelector('#logged-form')
     .addEventListener('submit', loginFormHandler);
-document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
