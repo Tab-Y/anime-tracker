12447d0c7cac333a609e10a516026fd9f558c4b5
Tab-Y <tlyoumans@gmail.com>
2022-11-16 20:23:08 -0500

    api/profile route working

--- controllers/api/profile-routes.js
+++ controllers/api/profile-routes.js
@@ -3,10 +3,24 @@ const router = require('express').Router();
 const { Main, TagId, Tags, Users, UserFavorite, Status } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-  res.render('profile', { 
+router.get('/', withAuth, async (req, res) => {
+  console.log(req.session.user_id)
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await Users.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+    });
 
-  });
-  });
+    const user = userData.get({ plain: true });
 
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+  
+  
   module.exports = router;
