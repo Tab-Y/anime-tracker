ce046c8136fd7bd5ecaa986284e46d2de778750b
Binny <ibinstock@outlook.com>
2022-11-16 18:38:00 -0800

    minor code touchups pt.2

--- public/js/createHTML.js
+++ public/js/createHTML.js
@@ -25,27 +25,22 @@ do {
     storePos.push(base);
 } while (mod6(viewLen*(cnt++)) != 0);
 
-window.onload = function() {
-    const insideEl = document.querySelector('.custom1');
-
-    let active = "";
-    for (let i = 0; i < storePos.length; i++) {
-        if (i == 0) {
-            active = "active";
-        } else {
-            active = "";
-        }
-
-        let guts = "";
-        for (let j = 0; j < viewLen; j++) {
-            guts += storeInner[mod6(storePos[i] + j)]
-        }
-
-        insideEl.innerHTML += `
-                    <div class="custom2 carousel-item ${active}">
-                        ${guts} 
-                    </div>
-        `;
+const insideEl = document.querySelector('.custom1');
+let active = "";
+for (let i = 0; i < storePos.length; i++) {
+    if (i == 0) {
+        active = "active";
+    } else {
+        active = "";
     }
+    let guts = "";
+    for (let j = 0; j < viewLen; j++) {
+        guts += storeInner[mod6(storePos[i] + j)]
+    }
+    insideEl.innerHTML += `
+                <div class="custom2 carousel-item ${active}">
+                    ${guts} 
+                </div>
+    `;
 }
 // Learned from https://bbbootstrap.com/snippets/bootstrap-5-bootstrap-5-carousel-vanilla-multiple-items-80120567#
