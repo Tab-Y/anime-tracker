0558aea8a2bd9562627189568c9627b14bcb42db
Binny <ibinstock@outlook.com>
2022-11-17 06:59:48 -0800

    made custom helper responsive to input size

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,29 +1,31 @@
 let storeInner = [];
 let storePos = [];
-
-const itemLen = 16; // context.length
 const viewLen = 4;
-let longShift = true;
 
-function mod6(input) {
-    return (input % itemLen);
+function mod6(input, base) {
+    return (input % base);
 }
 
-for (let i = 1; i <= itemLen; i++) {
-    let innerChunk = `
-                    <div class="col-md-3">
-                        <img src="https://via.placeholder.com/225x316?text=${i}" class="img-fluid">
-                    </div>
-    `;
+function preSetup(arrLength) {
+    const itemLen = arrLength; // context.length
+    let longShift = true;
 
-    storeInner.push(innerChunk);
+    for (let i = 1; i <= itemLen; i++) {
+        let innerChunk = `
+                        <div class="col-md-3">
+                            <img src="https://via.placeholder.com/225x316?text=${i}" class="img-fluid">
+                        </div>
+        `;
+
+        storeInner.push(innerChunk);
+    }
+
+    let cnt = 1;
+    do {
+        let base = mod6(viewLen*(cnt-1), itemLen);
+        storePos.push(base);
+    } while (mod6(viewLen*(cnt++), itemLen) != 0);
 }
-
-let cnt = 1;
-do {
-    let base = mod6(viewLen*(cnt-1));
-    storePos.push(base);
-} while (mod6(viewLen*(cnt++)) != 0);
 
 module.exports = {
     format_date: (date) => {
@@ -33,6 +35,7 @@ module.exports = {
     custom: (context, options) => {
         var ret = "";
         let active = "";
+        preSetup(context.length);
     
         for (let i = 0; i < storePos.length; i++) {
             if (i == 0) {
@@ -43,7 +46,7 @@ module.exports = {
     
             ret += `<div class="custom2 carousel-item ${active}">`;
             for (let j = 0; j < viewLen; j++) {
-                ret += options.fn(context[mod6(storePos[i] + j)]);
+                ret += options.fn(context[mod6(storePos[i] + j, context.length)]);
             }
             ret += "</div>";
         }
